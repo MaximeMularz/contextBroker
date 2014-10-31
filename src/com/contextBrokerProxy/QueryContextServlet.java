d79c7760ec497ea85f35efb0135cabcb8f06d840
maxime <maxime.mularz@4planet.eu>
2014-10-31 10:15:12 +0100

    Init

--- src/com/contextBrokerProxy/QueryContextServlet.java
+++ src/com/contextBrokerProxy/QueryContextServlet.java
@@ -16,6 +16,8 @@ public class QueryContextServlet extends HttpServlet {
 	/**
 	 * 
 	 */
+	
+	
 	private static final long serialVersionUID = 1L;
 	private static final String URL = "http://130.211.103.49:1026/NGSI10/queryContext";
 
